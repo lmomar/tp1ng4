9c01f54434bd223f919654a7a0b3c642147bfbee
Lmounir Omar <lmouniromar@gmail.com>
2018-03-25 11:51:44 +0100

    angular 4 tp1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router'
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './shared/layout/footer/footer.component';
+import {HeaderComponent} from './shared/layout/header/header.component';
+import {SidebarComponent} from './shared/layout/sidebar/sidebar.component';
+import {IndexComponent} from './admin/categories/index/index.component';
+
+const appRoutes: Routes = [
+    {path: '', component: IndexComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FooterComponent,
+        HeaderComponent,
+        SidebarComponent,
+        IndexComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(
+            appRoutes,
+            {enableTracing: false}
+        )
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
